dff41aebe6b2afb005e6c522792351ea4696e9d8
Muhammed-Shaheem <shaheemofcl@gmail.com>
2025-08-10 14:44:14 +0530

    Added Data Access layer to the app
    Added a sql data access class in Databases
    folder also implemented an interface for
    generic data access in case another database
    is needed for the app.

---
+++ HotelAppLibrary/Databases/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HotelAppLibrary.Databases;
+
+public interface IDataAccess
+{
+    List<T> LoadData<T, U>(string sqlStatement,
+                           U parameters,
+                           string connectionStringName,
+                           dynamic? options = null);
+    void SaveData<T>(string sqlStatement,
+                     T parameters,
+                     string connectionStringName,
+                     dynamic? options = null);
+}
---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace HotelAppLibrary.Databases;
+
+public class SqlDataAccess : IDataAccess
+{
+    private readonly IConfiguration config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoreProcedure = false)
+    {
+        config.GetConnectionString(connectionStringName);
+        CommandType cmdType = CommandType.Text;
+        if (isStoreProcedure == true)
+        {
+            cmdType = CommandType.StoredProcedure;
+        }
+        using (IDbConnection connection = new SqlConnection())
+        {
+            List<T> rows = connection.Query<T>(sqlStatement, parameters, commandType: cmdType).ToList();
+            return rows;
+        }
+
+    }
+
+    public void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoreProcedure)
+    {
+        CommandType cmdType = CommandType.Text;
+        if (isStoreProcedure == true)
+        {
+            cmdType = CommandType.StoredProcedure;
+        }
+        using (IDbConnection connection = new SqlConnection(config.GetConnectionString(connectionStringName)))
+        {
+            connection.Execute(sqlStatement, parameters, commandType: cmdType);
+        }
+    }
+}
+
+
