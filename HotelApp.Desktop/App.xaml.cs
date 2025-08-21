5a37262c42cd0cb99daeb8d766de693f7eca63a3
Muhammed-Shaheem <shaheemofcl@gmail.com>
2025-08-21 10:16:26 +0530

    Added a Wpf project in the solution.
    created a wpf project and added
    Dependecy injection service.

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using HotelAppLibrary.Data;
+using HotelAppLibrary.Databases;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Windows;
+
+namespace HotelApp.Desktop;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+ 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+        services.AddTransient<IDataAccess, SqlDataAccess>();
+        services.AddTransient<IDatabaseData, SqlData>();    
+       
+        services.AddTransient<MainWindow>();
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        IConfiguration builder = config.Build();
+
+        services.AddSingleton(builder);
+        var serviceProvider = services.BuildServiceProvider();
+        var mainWindow = serviceProvider.GetService<MainWindow>();
+
+        mainWindow?.Show();
+
+
+
+
+
+     
+
+    }
+}
+
