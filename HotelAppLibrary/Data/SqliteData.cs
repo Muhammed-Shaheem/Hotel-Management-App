e7d2db7d344fd532d3d90f23f0b8f6d2d1277180
Muhammed-Shaheem <shaheemofcl@gmail.com>
2025-08-26 15:48:51 +0530

    Added a search bookings method for Sqlite
    A method that search bookings table a
    retrive guests based on the todays date.

--- HotelAppLibrary/Data/SqliteData.cs
+++ HotelAppLibrary/Data/SqliteData.cs
@@ -100,6 +100,30 @@ public class SqliteData : IDatabaseData
 
     public List<FullBookingModel> SearchBookings(string lastName)
     {
-        throw new NotImplementedException();
+        string sql = @"select 
+	                 b.Id, [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate], [b].[CheckedIn], [b].[TotalCost],
+	                 [g].[FirstName], [g].[LastName],
+	                 [r].[RoomNumber], [r].[RoomTypeId],
+	                 [rt].[Title], [rt].[Description], [rt].[Price] 
+	                 from Guests g
+	                join Bookings b
+	                on b.GuestId = g.id
+	                join Rooms r
+	                on r.Id = b.RoomId
+	                join RoomTypes rt
+	                on rt.Id=r.RoomTypeId
+	                where LastName = @lastName
+	                and StartDate = @startDate
+	                and CheckedIn = 0";
+         List<FullBookingModel> output = db.LoadData<FullBookingModel, dynamic>(sql,
+                    new { lastName, startDate = DateTime.Now.Date },
+                    connectionStringName);
+        output.ForEach(x =>
+        {
+            x.TotalCost = x.TotalCost / 100;
+            x.Price = x.Price / 100;
+        });
+        return output;
+        
     }
 }
